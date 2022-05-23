26f056dd6fd99678d01fdc48af77908fab936b68
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-05-23 23:09:09 +0300

    feat: add solution for sixth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const {readdir} = require('fs/promises');
 const path = require('path');
-const folderPath = path.join(__dirname, 'files/');
+const assets = path.join(__dirname, 'files/');
 const newFolderPath = path.join(__dirname, 'newfiles/');
 const constants = require('constants');
 
@@ -17,10 +17,10 @@ const copyDir = async() => {
   }
   makeFolder(newFolderPath);
   try {
-    const files = await readdir(folderPath, {encoding: 'utf-8', withFileTypes: true});
+    const files = await readdir(assets, {encoding: 'utf-8', withFileTypes: true});
     for await (const file of files) {
       if (file.isFile()) {
-        await fs.promises.copyFile(path.join(folderPath, file.name), path.join(newFolderPath, file.name), constants.COPYFILE_FICLONE, callback);
+        await fs.promises.copyFile(path.join(assets, file.name), path.join(newFolderPath, file.name), constants.COPYFILE_FICLONE, callback);
       }
     }
   } catch(err) {
