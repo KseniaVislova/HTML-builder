48298fc7284321a5219351a5de460bd860085651
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-05-23 23:32:17 +0300

    fix: replace createReadStream to ReadStream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const pathToFile = path.join(__dirname, 'text.txt');
-const stream = fs.createReadStream(pathToFile, 'utf8');
+const stream = fs.ReadStream(pathToFile, 'utf8');
 
 stream.on('data', (data) => {
   (err) => {
