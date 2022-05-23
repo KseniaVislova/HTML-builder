5b46c29b40d1fa2fa8eeb52e5b685d01b937463d
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-05-23 23:55:46 +0300

    refactor: change solution for third task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,20 +1,19 @@
 const path = require('path');
-const {readdir} = require('fs/promises');
 const fs = require('fs');
 const folderPath = path.join(__dirname, 'secret-folder/');
 
-const getFiles = async() => {
-  try {
-    const files = await readdir(folderPath, {encoding: 'utf-8', withFileTypes: true});
-    for await (const file of files) {
+const getFiles = () => {
+  fs.readdir(folderPath, {encoding: 'utf-8', withFileTypes: true}, (error, files) => {
+    if (error) throw error;
+    for (const file of files) {
       if (file.isFile()) {
-        let size =  await fs.promises.stat(path.join(folderPath, file.name))
-        console.log(`${file.name.split('.')[0]} - ${path.extname(file.name).split('.')[1]} - ${size.size / 1024} кб`)
+        fs.stat(path.join(folderPath, file.name), (error, stats) => {
+          if (error) throw error;
+          console.log(`${file.name.split('.')[0]} - ${path.extname(file.name).split('.')[1]} - ${stats.size / 1024} кб`);
+        })
       }
     }
-  } catch (err) {
-    console.error(err);
-  }
+  });
 }
 
-getFiles()
+getFiles();
