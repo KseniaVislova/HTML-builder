bc2c7d86b9ea7f42b435001ee039f68116f8d994
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-05-23 17:09:24 +0300

    feat: add solution for fourth task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -16,4 +16,5 @@ const getFiles = async() => {
     console.error(err);
   }
 }
+
 getFiles()
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const fs = require('fs');
+const {readdir} = require('fs/promises');
+const path = require('path');
+const folderPath = path.join(__dirname, 'files/');
+const newFolderPath = path.join(__dirname, 'newfiles/');
+const constants = require('constants');
 
+const makeFolder = (path) => {
+  fs.mkdir(path, { recursive: true }, err => {
+    if (err) throw err;
+  });
+}
+
+const copyDir = async() => {
+  function callback() {
+    if (err) throw err;
+  }
+  makeFolder(newFolderPath);
+  try {
+    const files = await readdir(folderPath, {encoding: 'utf-8', withFileTypes: true});
+    for await (const file of files) {
+      if (file.isFile()) {
+        await fs.promises.copyFile(path.join(folderPath, file.name), path.join(newFolderPath, file.name), constants.COPYFILE_FICLONE, callback);
+      }
+    }
+  } catch(err) {
+    console.error(err);
+  }
+}
+
+copyDir();
